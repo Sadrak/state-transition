7d74afd4907b92fb238abcd9df51c5e5a43c41a5
Felix Ostmann <sadrak@cpan.org>
2011-06-21 13:49:59 +0200

    added boilerplate-docu and Changes for version .01

--- lib/State/Transition.pm
+++ lib/State/Transition.pm
@@ -1,5 +1,28 @@
+
 use strict;
 use warnings;
 package State::Transition;
+# ABSTRACT: Kind of a state-machine
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+=method new
+
+=cut
+
+sub new {
+}
+
+=head1 SEE ALSO
+
+=for :list
+* L<State::Transition>;
+
+=cut
 
 1;
+
